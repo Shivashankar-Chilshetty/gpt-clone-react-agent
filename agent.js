17ef80a210b0d398fb7d55b3a3a3963947f57214
Aishwarya-Chilshetty <chilshetty77@gmail.com>
2025-10-01 10:13:31 +0530

    implemented visual graph - which shows application flow

--- agent.js
+++ agent.js
@@ -3,6 +3,8 @@ import { ChatGroq } from "@langchain/groq";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { TavilySearch } from "@langchain/tavily";
 import { z } from "zod";
+import { writeFileSync } from "node:fs";
+import readline from "node:readline/promises";
 
 async function main() {
     //creating groq model
@@ -12,16 +14,16 @@ async function main() {
     });
 
     const search = new TavilySearch({
-         maxResults: 3,
-         topic: "general"
-     });
+        maxResults: 3,
+        topic: "general"
+    });
 
     //insider tool function we have 2 parameters, function & metadata object
     const calendarEvents = tool(
         async ({ query }) => {
-           
+
             return JSON.stringify([
-                { title: "Meeting with Sujoy", time: "7:00 PM", date: "30 sept 2025", location: "Google Meet"}
+                { title: "Meeting with Sujoy", time: "7:00 PM", date: "30 sept 2025", location: "Google Meet" }
             ]);
         },
         {
@@ -39,20 +41,38 @@ async function main() {
         tools: [search, calendarEvents],
     });
 
-    //calling the agent
-    const result = await agent.invoke({
-        messages: [
-             {
-                role: `system`,
-                content: `You are a helpful personal assistant. Use provided tools to get the information needed to answer the user's question. Current date & time: ${new Date().toUTCString}`
-            },
-            {
-                role: "user",
-                content: "Hi, Do I have any meetings tomorrow?",
-            },
-        ],
-    });
-    console.log('Assistant:', result.messages[result.messages.length - 1].content)
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+    while (true) {
+        const userQuery = await rl.question("You: ");
+        if(userQuery.toLowerCase() == "bye") break;
+        //calling the agent
+        const result = await agent.invoke({
+            messages: [
+                {
+                    role: `system`,
+                    content: `You are a helpful personal assistant. Use provided tools to get the information needed to answer the user's question. Current date & time: ${new Date().toUTCString}`
+                },
+                {
+                    role: "user",
+                    content: userQuery,
+                },
+            ],
+        });
+        console.log('Assistant:', result.messages[result.messages.length - 1].content)
+    }
+
+    rl.close();
+
+    const drawableGraphGraphState = await agent.getGraphAsync();
+
+    const graphStateImage = await drawableGraphGraphState.drawMermaidPng();
+    const graphStateArrayBuffer = await graphStateImage.arrayBuffer();
+
+    const filePath = "./graphState.png";
+    writeFileSync(filePath, new Uint8Array(graphStateArrayBuffer));
+
+    
 }
 
 main();
